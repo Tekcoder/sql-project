0f68ccf87c42098b42cab968d61bd0799e4b0fcd
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-19 00:28:11 +0100

    added the first answer in the bbc quiz section

---
+++ 4_BBC QUIZ.sql
@@ -0,0 +1,15 @@
+-- name 	continent 	area 	population 	gdp
+-- Afghanistan 	South Asia 	652225 	26000000 	
+-- Albania 	Europe 	28728 	3200000 	6656000000
+-- Algeria 	Middle East 	2400000 	32900000 	75012000000
+-- Andorra 	Europe 	468 	64000 	
+-- Brazil 	South America 	8550000 	182800000 	564852000000
+-- Colombia 	South America 	1140000 	45600000 	
+-- Nauru 	Asia-Pacific 	21 	9900 	
+-- Uzbekistan 	Central Asia 	447000 	26000000 	
+
+-- 1. Select the code which gives the name of countries beginning with U 
+Answer:
+SELECT name
+  FROM world
+ WHERE name LIKE 'U%'
