1b58c64dca22ff85acce3e0145d5a4fe57546660
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-18 23:01:36 +0100

    added the first answer

---
+++ 1_SELECT basics.sql
@@ -0,0 +1,8 @@
+-- Instructions
+-- The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) should be in 'single quotes';
+
+--1- Modify it to show the population of Germany
+Answer:
+SELECT population FROM world
+  WHERE name = 'Germany'
+
