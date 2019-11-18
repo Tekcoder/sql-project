1889070e48fa86a28fa474d05b1bd5aee868cd40
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-18 23:55:09 +0100

    added the ninth answer in the select_world

--- 3_SELECT FROM WORLD.sql
+++ 3_SELECT FROM WORLD.sql
@@ -43,3 +43,10 @@ WHERE area > 3000000 OR population > 250000000
 Answer:
 SELECT name, population, area FROM world
 WHERE  area > 3000000 XOR population > 250000000 
+
+--9- Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+-- For South America show population in millions and GDP in billions both to 2 decimal places.
+-- Millions and billions
+Answer:
+SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world
+WHERE continent = 'South America'
