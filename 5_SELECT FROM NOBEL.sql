9f1641e55e468a949edbce096d6ffe2ce77e4070
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-19 13:21:31 +0100

    completed the seventh task

--- 5_SELECT FROM NOBEL.sql
+++ 5_SELECT FROM NOBEL.sql
@@ -27,3 +27,8 @@ WHERE subject = 'Literature' AND yr >= 1980 AND yr <= 1989;
 SELECT *
 FROM nobel
 WHERE winner = 'Theodore Roosevelt' OR winner = 'Woodrow Wilson' OR winner = 'Jimmy Carter' OR winner = 'Barack Obama';
+
+-- 7) Show the winners with first name John
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'John%';
