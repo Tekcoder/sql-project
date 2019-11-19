aec4dee9b3593d6845df86da4ea5c5536d51357c
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-19 23:08:31 +0100

    added the second task

--- 8_Nested_SELECT_Quiz.sql
+++ 8_Nested_SELECT_Quiz.sql
@@ -10,4 +10,6 @@
 
 Answer:
  SELECT region, name, population FROM bbc x WHERE population <= ALL (SELECT population FROM bbc y WHERE y.region=x.region AND population>0)
- 
+
+--  2. Select the code that shows the countries belonging to regions with all populations over 50000 
+ SELECT name,region,population FROM bbc x WHERE 50000 < ALL (SELECT population FROM bbc y WHERE x.region=y.region AND y.population>0)
