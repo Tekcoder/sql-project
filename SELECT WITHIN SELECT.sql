1cbc05131471f0e3b541092202cf15def159e520
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-19 16:20:52 +0100

    added second task

--- SELECT WITHIN SELECT.sql
+++ SELECT WITHIN SELECT.sql
@@ -7,3 +7,10 @@ SELECT name FROM world
   WHERE population >
      (SELECT population FROM world
       WHERE name='Russia')
+
+-- 2- Show the names of countries in Europe with a per capita GDP greater than 'United Kingdom'.
+-- Per Capita GDP
+-- The per capita GDP is the gdp/population
+Answer:
+SELECT name FROM world
+WHERE continent = 'Europe' AND gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom')
