c1b071f6003c2169535d043faaaeb4e13d3d0d4c
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-19 14:49:21 +0100

    completed the first answer

---
+++ 6_SELECT NOBEL_QUIZ.sql
@@ -0,0 +1,12 @@
+-- yr 	subject 	winner
+-- 1960 	Chemistry 	Willard F. Libby
+-- 1960 	Literature 	Saint-John Perse
+-- 1960 	Medicine 	Sir Frank Macfarlane Burnet
+-- 1960 	Medicine 	Peter Medawar
+-- 1960 	Physics 	Donald A. Glaser
+-- 1960 	Peace 	Albert Lutuli
+-- ...
+-- 1. Pick the code which shows the name of winner's names beginning with C and ending in n 
+
+SELECT winner FROM nobel
+ WHERE winner LIKE 'C%' AND winner LIKE '%n'
