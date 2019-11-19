d62fa1e939b7385a6208a5a2080704fad4255f3f
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-19 13:20:27 +0100

    completed the fifth task

--- 5_SELECT FROM NOBEL.sql
+++ 5_SELECT FROM NOBEL.sql
@@ -17,3 +17,8 @@ WHERE winner = 'Albert Einstein';
 SELECT winner
 FROM nobel
 WHERE subject = 'Peace' AND yr >= 2000;
+
+-- 5) Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Literature' AND yr >= 1980 AND yr <= 1989;
