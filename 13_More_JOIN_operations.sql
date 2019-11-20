21901b7a9437f1f28a52af47bbc281397f978b1a
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-20 20:43:30 +0100

    added tenth task

--- 13_More_JOIN_operations.sql
+++ 13_More_JOIN_operations.sql
@@ -66,3 +66,15 @@ WHERE name = 'Harrison Ford';
 -- 9.
 
 -- List the films where 'Harrison Ford' has appeared - but not in the starring role. [Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role] 
+
+SELECT title
+FROM casting JOIN actor ON actorid = actor.id JOIN movie ON movieid = movie.id
+WHERE name = 'Harrison Ford' AND ord != 1;
+
+-- 10.
+
+-- List the films together with the leading star for all 1962 films. 
+
+SELECT title, name
+FROM casting JOIN actor ON actorid = actor.id JOIN movie ON movieid = movie.id
+WHERE yr = 1962 AND ord = 1;
