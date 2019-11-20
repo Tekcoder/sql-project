e7256993755b27da4f1fcb82b0d0a6e48b8dbe8f
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-21 00:00:04 +0100

    added second task

--- 14_JOIN_Quiz.sql
+++ 14_JOIN_Quiz.sql
@@ -3,3 +3,9 @@
 SELECT name
   FROM actor INNER JOIN movie ON actor.id = director
  WHERE gross < budget
+
+--  2. Select the correct example of JOINing three tables 
+
+SELECT *
+  FROM actor JOIN casting ON actor.id = actorid
+  JOIN movie ON movie.id = movieid
