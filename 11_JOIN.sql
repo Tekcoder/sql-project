7b8d3fabd37f2981b30eb7e7dd2747331f355b4e
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-20 15:45:07 +0100

    added the eigth task

--- 11_JOIN.sql
+++ 11_JOIN.sql
@@ -77,3 +77,13 @@ WHERE coach = 'Fernando Santos'
 SELECT player
 FROM game JOIN goal ON (id=matchid)
 WHERE stadium = 'National Stadium, Warsaw';
+
+
+-- 8.
+-- The example query shows all goals scored in the Germany-Greece quarterfinal.
+
+-- Instead show the name of all players who scored a goal against Germany.
+
+SELECT DISTINCT player
+  FROM game JOIN goal ON matchid = id 
+    WHERE (team1='GER' OR team2='GER') AND teamid != 'GER'
