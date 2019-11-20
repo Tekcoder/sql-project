acecc50c8b9c96ea66e9b71d30a54f2f84faa9d7
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-20 15:41:15 +0100

    added the fourth task

--- 11_JOIN.sql
+++ 11_JOIN.sql
@@ -36,3 +36,13 @@ WHERE id = 1012
 SELECT player,teamid,stadium, mdate
   FROM game JOIN goal ON (id=matchid)
 WHERE teamid = 'GER'
+
+-- 4.
+
+-- Use the same JOIN as in the previous question.
+
+-- Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+
+SELECT team1, team2, player
+FROM game JOIN goal ON (id=matchid)
+WHERE player LIKE 'Mario%'
