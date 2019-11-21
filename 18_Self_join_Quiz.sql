317899a2a543ceb17b06c98675e4d4964c9b5b54
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-21 18:25:45 +0100

    added the third value

--- 18_Self_join_Quiz.sql
+++ 18_Self_join_Quiz.sql
@@ -14,4 +14,10 @@ SELECT S2.id, S2.name, R2.company, R2.num
    AND R1.company=R2.company AND R1.num=R2.num
    AND R2.stop=S2.id AND R2.num='2A'
 
-   
+-- 3. Select the code that shows the services available from Tollcross? 
+
+SELECT a.company, a.num, stopa.name, stopb.name
+  FROM route a JOIN route b ON (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+ WHERE stopa.name='Tollcross'
