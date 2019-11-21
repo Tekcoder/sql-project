e5b591bac4fcd86596309eaed2ce8c3383cebf6f
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-21 17:24:27 +0100

    added the teth value

--- 15_Using_Null.sql
+++ 15_Using_Null.sql
@@ -77,4 +77,19 @@ CASE WHEN teacher.dept = 1
      ELSE 'Art'
      END
 FROM teacher
-     
+
+
+-- 10.
+
+-- Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise. 
+
+SELECT teacher.name,
+CASE WHEN teacher.dept = 1
+     THEN 'Sci'
+     WHEN teacher.dept = 2
+     THEN 'Sci'
+     WHEN teacher.dept = 3
+     THEN 'Art'
+     ELSE 'None'
+     END
+FROM teacher
