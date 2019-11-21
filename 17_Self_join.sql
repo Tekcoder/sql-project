2db2ea020d318c021d1130ffe0103638bb155f15
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-21 18:00:46 +0100

    added the third value

--- 17_Self_join.sql
+++ 17_Self_join.sql
@@ -12,3 +12,11 @@ FROM stops
 SELECT id
 FROM stops
 WHERE name = 'Craiglockhart' 
+
+-- 3.
+
+-- Give the id and the name for the stops on the '4' 'LRT' service. 
+
+SELECT id, name
+FROM stops JOIN route ON (stops.id = route.stop)
+WHERE num = '4' AND company = 'LRT'
