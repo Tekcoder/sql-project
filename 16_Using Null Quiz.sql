527c4b444f49dfd350b74b2c5b9999581d206ed2
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-21 17:43:41 +0100

    added the third value

--- 16_Using Null Quiz.sql
+++ 16_Using Null Quiz.sql
@@ -20,3 +20,7 @@
 --  2. Select the correct statement that shows the name of department which employs Cutflower - 
 
  SELECT dept.name FROM teacher JOIN dept ON (dept.id = teacher.dept) WHERE teacher.name = 'Cutflower'
+
+--  3. Select out of following the code which uses a JOIN to show a list of all the departments and number of employed teachers 
+
+ SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept GROUP BY dept.name
