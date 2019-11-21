b52e26df6efd54844df4e898b6f6f3643cdb5c9c
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-21 17:20:43 +0100

    added the nineth value

--- 15_Using_Null.sql
+++ 15_Using_Null.sql
@@ -64,3 +64,17 @@ SELECT dept.name, COUNT(teacher.name)
 FROM teacher RIGHT JOIN dept ON(teacher.dept = dept.id)
 
 GROUP BY dept.name
+
+-- 9.
+
+-- Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise. 
+
+SELECT teacher.name,
+CASE WHEN teacher.dept = 1
+     THEN 'Sci'
+     WHEN teacher.dept = 2
+     THEN 'Sci'
+     ELSE 'Art'
+     END
+FROM teacher
+     
